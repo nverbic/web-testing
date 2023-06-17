072c6ff60d3ac6b258cdf35132de54fa2e138820
Nevena Verbic <nevena.verbic@gmail.com>
2023-06-17 11:20:00 +0200

    Problem: page sometimes not loaded. Solution: if the page is not loaded Timeout except. is thrown after trying to locate accept cookies button. In the catch block the page is refreshed.

--- pages/log_in.py
+++ pages/log_in.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import TimeoutException
 
 class LogInPage:
     URL = 'https://www.linkedin.com/home/'
@@ -18,13 +19,20 @@ class LogInPage:
 
     def load(self):
         self.browser.maximize_window()
+        print("\nLoad URL")
         self.browser.get(self.URL)
 
     def accept_cookies(self):
         print("Accept cookies")
-        accept_button = WebDriverWait(self.browser, 10).until(expected_conditions.element_to_be_clickable(self.ACCEPT_COOKIES_BUTTON))
+        try:
+            accept_button = WebDriverWait(self.browser, 10).until(expected_conditions.element_to_be_clickable(self.ACCEPT_COOKIES_BUTTON))
+        except TimeoutException:
+            print("Timeout loading page")
+            self.browser.refresh()
+            print("Refresh page")
+            accept_button = WebDriverWait(self.browser, 10).until(expected_conditions.element_to_be_clickable(self.ACCEPT_COOKIES_BUTTON))
+            print("Accept cookies after refresh")
         # accept_button = browser.find_element(By.XPATH, '//*[@class="artdeco-global-alert-action__wrapper"]/button[1]')
-        # accept_button = browser.find_element(By.XPATH, '//*[@class="artdeco-global-alert-action__wrapper"]/button[@action-type=\'ACCEPT\']')
         # accept_button = browser.find_element(By.XPATH, '//*[@class="artdeco-global-alert-action__wrapper"]/button[@data-control-name="ga-cookie.consent.accept.v4"]')
         # print(accept_button.get_attribute('action-type'))
         accept_button.click()

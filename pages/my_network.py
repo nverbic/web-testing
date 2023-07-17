bc619b40c97a7605e68e6fc59194a33efd22ce31
Nevena Verbic <nevena.verbic@gmail.com>
2023-07-17 11:36:30 +0200

    Add new file in tests. File name: test_my_network_suggested_connections.py Test case: test_close_first_suggested_connection() for close button of the first suggested connection. Add corresponding methods in MyNetwork class.

--- pages/my_network.py
+++ pages/my_network.py
@@ -9,15 +9,47 @@ from pages.connections import Connections
 
 class MyNetwork:
     MANAGE_MY_NETWORK = (By.CLASS_NAME, "mn-community-summary__section")
+    SUGGESTED_PEOPLE_GROUP = (By.XPATH, "//main/ul/li[1]/ul")
+    SUGGESTED_PEOPLE_LIST = (By.TAG_NAME, "li")
+    SUGGESTED_PEOPLE_ITEM_CLOSE_BUTTON = (By.XPATH, './/button[contains(@class,"artdeco-card__dismiss")]')
+    SUGGESTED_PEOPLE_ITEM_LINK = (By.XPATH, './/a[contains(@class,"discover-entity-type-card__link")]')
 
     def __init__(self, browser):
         self.browser = browser
+        self.deleted_suggested_connection_href = ""
 
     def check_manage_my_network_panel_is_visible(self):
         print("Check \"Manage my network\" panel is visible")
-        manage_network_panel = WebDriverWait(self.browser, 10).until(
+        WebDriverWait(self.browser, 10).until(
             expected_conditions.visibility_of_element_located(self.MANAGE_MY_NETWORK))
         return ManageMyNetworkPanel(self.browser)
+
+    def find_href_in_suggested_people_list(self, href=None):
+        if href is None:
+            href = self.deleted_suggested_connection_href
+        suggested_people_list = self.get_suggested_people_list()
+        for li in suggested_people_list:
+            if li.get_attribute('href') == href:
+                return li
+        return None
+
+    def close_suggested_people_list_item(self, item_number=0):
+        self.deleted_suggested_connection_href = ""
+        suggested_people_list = self.get_suggested_people_list()
+        print("Get person's href")
+        self.deleted_suggested_connection_href = suggested_people_list[item_number].\
+            find_element(*self.SUGGESTED_PEOPLE_ITEM_LINK).get_attribute('href')
+        print("Close the suggested person")
+        suggested_people_list[item_number].find_element(*self.SUGGESTED_PEOPLE_ITEM_CLOSE_BUTTON).click()
+        return self
+
+    def get_suggested_people_list(self):
+        print("Get suggested people ul element")
+        suggested_people_ul_elem = WebDriverWait(self.browser, 10). \
+            until(expected_conditions.visibility_of_element_located(self.SUGGESTED_PEOPLE_GROUP))
+        print("Get suggested people list of li elements")
+        suggested_people_list = suggested_people_ul_elem.find_elements(*self.SUGGESTED_PEOPLE_LIST)
+        return suggested_people_list
 
 
 class ManageMyNetworkPanel(MyNetwork):
---
+++ tests/test_my_network_suggested_connections.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+# home_page: Log in and return home_page before every test
+def test_close_first_suggested_connection(browser, username, password, home_page):
+    deleted_person_href = home_page.click_my_network_link().\
+        close_suggested_people_list_item().\
+        find_href_in_suggested_people_list()
+
+    # Verify there is no suggested person with the deleted_person_href
+    assert deleted_person_href is None
+    browser.close()

770da24e71d3d914a59f9da720af90e900886a7a
Nevena Verbic <nevena.verbic@gmail.com>
2023-09-07 10:55:02 +0200

    Changes in test_two_profiles_sending_messages.py In test_two_profiles_sending_messages:  - In the test open two browsers simultaneously and login with two    different users. Messaging class:  - Add new method that checks the new message in the last conversation    thread.

--- pages/messaging.py
+++ pages/messaging.py
@@ -6,11 +6,14 @@ from selenium.webdriver.support import expected_conditions
 class Messaging:
     MESSAGING_PANEL = (By.XPATH, '//ul[contains(@class,"msg-conversations-container__conversations-list")]')
     MESSAGING_LIST = (By.TAG_NAME, "li")
+    NEW_CONVERSATION_THREAD_LINK = (By.XPATH, '//li//a[contains(@class, "msg-conversation-listitem__link")]')
+    NEW_CONVERSATION_THREAD_LAST_MESSAGE = (By.XPATH, "//li//p")
 
     def __init__(self, browser):
         self.browser = browser
 
     # Check for the specific message in the list of all messages
+    # TODO: Check when there are many messages and need scrolling
     def check_message_is_received(self, first_name, last_name, message_text):
         message_data = [first_name, last_name, message_text]
         messages_list = self.get_messages_list()
@@ -18,6 +21,23 @@ class Messaging:
             if all([data in message.text for data in message_data]):
                 print(f"Message received: \n{message.text} ")
                 return True
+        return False
+
+    def check_last_message_is_received(self, first_name, last_name, message_text):
+        message_contact = [first_name, last_name]
+        # In the Messaging list of the conversations, the last conversation, with the newly received message,
+        # is displayed at the top of the list.
+        last_received_message = WebDriverWait(self.browser, 10). \
+            until(expected_conditions.element_to_be_clickable(self.NEW_CONVERSATION_THREAD_LINK))
+        # If the contact details are correct, check if the new message in the conversation thread is the same as in the
+        # parameter "message_text".
+        # NOTE: The conversation thread is automatically opened and focused (no need (and not possible) to click
+        # on the contact in the Messaging list)
+        if all([data in last_received_message.text for data in message_contact]):
+            print(f"Message received: \n{last_received_message.text} ")
+            found_last_message_in_the_conversation = WebDriverWait(self.browser, 10). \
+                until(expected_conditions.text_to_be_present_in_element(self.NEW_CONVERSATION_THREAD_LAST_MESSAGE, message_text))
+            return found_last_message_in_the_conversation
         return False
 
     # Get a list of all messages
@@ -29,5 +49,3 @@ class Messaging:
         # Create list
         messages_list = messaging_panel.find_elements(*self.MESSAGING_LIST)
         return messages_list
-
-
--- tests/test_two_profiles_sending_messages.py
+++ tests/test_two_profiles_sending_messages.py
@@ -13,25 +13,28 @@ def test_send_message_to_connection_from_connections_panel(home_page, browser, f
     if connection_url is not None:
         connection_url = request.getfixturevalue(connection_url)
 
-    # TODO: Private browsers, both opened one next to another
+    # TODO:Check how to open browsers one next to another
     local_time = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
     message_text = f'Hi! Message sent at {local_time}.'
+
+    # Open the second browser and login with the different user
+    login_second_page = LogInPage(browser_second, username_user_2, password_user_2)
+    home_page_second_user = login_second_page.load(). \
+        accept_cookies(). \
+        login()
 
     # Click on My Network link and then on the Connections link and send the message to the first connection
     home_page.click_my_network_link(). \
         check_manage_my_network_panel_is_visible(). \
         click_connections_link().\
         send_message_to_connection(message_text, connection_url)
-    browser.close()
 
-    login_second_page = LogInPage(browser_second, username_user_2, password_user_2)
-    home_page_second_user = login_second_page.load(). \
-        accept_cookies(). \
-        login()
-    result = home_page_second_user.\
-        click_messaging_link().\
-        check_message_is_received(first_name, last_name, message_text)
+    result = home_page_second_user. \
+        click_messaging_link(). \
+        check_last_message_is_received(first_name, last_name, message_text)
 
     # TODO: Test and send message about the error
     assert result is True
+
+    browser.close()
     browser_second.close()
